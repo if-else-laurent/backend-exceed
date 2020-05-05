5e997b47d76d4f0e2dfdbb3251b6af0b8e9d7e4d
Mart Miller <apofiuz@gmail.com>
2020-05-05 12:39:55 +0300

    delete if ENV

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ app.use(cors());
 app.use('/', require('./routes/user.routes'))
 app.use('/auth', require('./routes/auth.routes'))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+//   })
+// }
 
 // const PORT = config.get('port') || 5000
 const PORT = process.env.PORT || 5000
